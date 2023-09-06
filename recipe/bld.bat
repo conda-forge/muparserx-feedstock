007edab763f3c88e7cac29f568019a9b31538cb1
Will Shanks <willshanks@us.ibm.com>
2023-09-06 09:46:43 -0400

    Add missing Windows build script

---
+++ recipe/bld.bat
@@ -0,0 +1,11 @@
+mkdir build_
+cd build_
+
+cmake -LAH -G"NMake Makefiles" ^
+    -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+    -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+    ..
+if errorlevel 1 exit 1
+
+cmake --build . --config Release --target install
+if errorlevel 1 exit 1
