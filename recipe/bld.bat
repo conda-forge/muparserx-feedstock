2bef47bf0e9ba148f1267ea56f24408f4e100d2e
Will Shanks <wshaos@posteo.net>
2023-09-07 14:46:32 -0400

    Use Release build type on Windows (#6)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,8 +2,9 @@ mkdir build_
 cd build_
 
 cmake -LAH -G"NMake Makefiles" ^
-    -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
-    -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+    -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
+    -DCMAKE_PREFIX_PATH:PATH=%LIBRARY_PREFIX% ^
+    -DCMAKE_BUILD_TYPE:STRING=Release ^
     ..
 if errorlevel 1 exit 1
 
